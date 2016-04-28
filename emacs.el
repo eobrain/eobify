3e97430dee8b1ba1699257df61acb80e84de7cf6
Eamonn O'Brien-Strain <eobrain@google.com>
2016-04-28 10:56:43 -0700

    keys and package archives

--- emacs.el
+++ emacs.el
@@ -13,6 +13,10 @@
 ;; CONTROL-Z for undo
 (global-set-key (kbd "C-z") 'undo)
 
+;; Function keys
+(global-set-key (kbd "<f5>") 'compile)
+(global-set-key (kbd "<f6>") 'next-error)
+
 ;; Spellcheck comments
 (add-hook 'c++-mode-hook
           (lambda ()
@@ -20,3 +24,8 @@
             ))
 ;; restricts spellcheck to comments
 (setq flyspell-prog-text-faces '(font-lock-comment-face font-lock-doc-face))
+
+;; Common packages
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("marmalade" . "https://marmalade-repo.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
